1a6b8f259c67441bb4642d9f9ce10a62520401ab
Dylan Blakemore <dylan.blakemore@gmail.com>
2023-07-20 16:53:19 +0200

    Typespecs

--- lib/exbox/metrics/metrics.ex
+++ lib/exbox/metrics/metrics.ex
@@ -1,10 +1,12 @@
 defmodule Exbox.Metrics do
   alias Exbox.Metrics.MetricHandler
 
+  @spec attach_controller_metrics(binary()) :: :ok
   def attach_controller_metrics(name) do
     attach_telemetry(name, [:phoenix, :endpoint, :stop])
   end
 
+  @spec attach_telemetry(binary(), list(atom())) :: :ok
   def attach_telemetry(event, params) do
     :ok =
       :telemetry.attach(
