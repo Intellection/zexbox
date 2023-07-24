7f1c9502cae497810033fd4324900e4949e62faf
Dylan Blakemore <dylan.blakemore@gmail.com>
2023-07-24 21:23:55 +0200

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exbox.MixProject do
   def project do
     [
       app: :exbox,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
