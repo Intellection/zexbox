9ba01537b5936febbe7b7a4a2490a029766fdd85
Kent Hawkings <kent.hawkings@zappistore.com>
2023-10-11 10:58:03 +0200

    Bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Zexbox.MixProject do
   def project do
     [
       app: :zexbox,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       dialyzer: [plt_add_apps: [:mix, :ex_unit]],
