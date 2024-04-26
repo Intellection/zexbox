215816722cf5d301a0f41a97f3044d77155eb2f3
Kent Hawkings <kent.hawkings@zappistore.com>
2024-04-26 13:24:07 +0200

    Don't need this anymore since LD 3.1

--- lib/zexbox/flags.ex
+++ lib/zexbox/flags.ex
@@ -54,15 +54,6 @@ defmodule Zexbox.Flags do
     sdk_key
     |> String.to_charlist()
     |> :ldclient.start_instance(tag, parse_config(config))
-  rescue
-    # Well this is annoying. When attempting to start the LDclient twice it errors out rather
-    # than returning a map of the form {:error, {:already_started, #PID<0.602.0>}} (which is the desired behaviour).
-    # Having a look at the source code  shows that this is a known issue since there is just a TODO stating:
-    # 'check if Tag already exists and return already_started error'.
-    # The MatchError is a struct of the form %MatchError{term: {:error, {:already_started, #PID<0.602.0>}}
-    # so we just return the 'term' attribute, giving us what we want.
-    e in MatchError ->
-      e.term
   end
 
   @doc """
