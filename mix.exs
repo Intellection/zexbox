9eef190c1cacba5f49d8f082213e56dfe7741b71
Kent Hawkings <kent.hawkings@zappistore.com>
2024-07-04 09:50:36 +0200

    Version 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Zexbox.MixProject do
   def project do
     [
       app: :zexbox,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       dialyzer: [plt_add_apps: [:mix, :ex_unit]],
