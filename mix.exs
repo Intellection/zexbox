44abfd331c296a7aa7dc1efdb722b8bd442bc870
jasperchess <jasper.chesselet@zappistore.com>
2024-12-04 10:51:13 +0200

    minor bumps

--- mix.exs
+++ mix.exs
@@ -34,14 +34,14 @@ defmodule Zexbox.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:doctor, "~> 0.21.0", only: [:dev, :test]},
+      {:doctor, "~> 0.22.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:instream, "~> 2.2"},
       {:ldclient, "~> 3.0.0", hex: :launchdarkly_server_sdk},
       {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
       {:mock, "~> 0.3.0", only: :test},
       {:sobelow, "~> 0.8", only: [:dev, :test]},
-      {:telemetry, "~> 1.2.1"}
+      {:telemetry, "~> 1.3"}
     ]
   end
 
