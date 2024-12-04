370367ead00e9fbf285b55efac027917f8acb938
jasperchess <jasper.chesselet@zappistore.com>
2024-12-04 11:12:43 +0200

    bump version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Zexbox.MixProject do
   def project do
     [
       app: :zexbox,
-      version: "1.3.0",
+      version: "1.4.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       dialyzer: [plt_add_apps: [:mix, :ex_unit]],
